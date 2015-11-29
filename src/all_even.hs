c186317a6258f50edd29ad11eea9a7663fda16e1
Eric Tobin <eatobin@gmail.com>
2015-11-29 15:44:07 -0600

    test

--- src/all_even.hs
+++ src/all_even.hs
@@ -1,4 +1,5 @@
 module AllEven where
-    allEven :: [Integer] -> [Integer]
-    allEven [] = []
-    allEven (h:t) = if even h then h:allEven t else allEven t
+
+allEven :: [Integer] -> [Integer]
+allEven [] = []
+allEven (h:t) = if even h then h:allEven t else allEven t
