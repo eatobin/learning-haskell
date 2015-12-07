1e7a578ad8c415ba516635ee3dab40986d726048
Eric Tobin <eatobin@gmail.com>
2015-12-07 13:29:41 -0600

    add learn-you

---
+++ src/learn-you/ch1/baby.hs
@@ -0,0 +1,14 @@
+module Learn where
+
+doubleMe x = x + x
+
+doubleUs x y = x * 2 + y * 2
+
+doubleSmallNumber x = if x > 100
+                        then x
+                        else x * 2
+
+doubleSmallNumber' x =  (if x > 100
+                          then x
+                          else x * 2)
+                        + 1
