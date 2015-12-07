fa234190a4a705dd4865d2221628efcabdf515d0
eatobin <eatobin@gmail.com>
2015-12-07 16:48:26 -0600

    ch01

---
+++ src/learn-you/ch01/baby.hs
@@ -0,0 +1,15 @@
+module Learn where
+
+doubleMe x = x + x
+
+doubleUs x y = x * 2 + y * 2
+
+doubleSmallNumber x = if x > 100
+  then x
+  else x * 2
+
+doubleSmallNumber' x = (if x > 100
+  then x
+  else x * 2)
++ 1
+-- to page 8
