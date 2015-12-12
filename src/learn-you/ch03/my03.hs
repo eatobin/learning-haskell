3bd471d5c8bef42a4b6be1a984326ec84f298804
Eric Tobin <eatobin@gmail.com>
2015-12-12 10:51:10 -0600

    ch 3 progress

--- src/learn-you/ch03/my03.hs
+++ src/learn-you/ch03/my03.hs
@@ -1,6 +1,29 @@
 module My03 where
 
-myAdd :: Int -> Int -> Int
+myAdd :: Int
 myAdd = a + b
   where a = 5
         b = 6
+
+myAdd' :: Int -> Int -> Int
+myAdd' a b = x + y
+  where x = a * b
+        y = div a b
+
+myAdd'' :: Int -> Int -> Int
+myAdd'' a b =
+  let x = a * b
+      y = div a b
+  in x + y
+
+myAddSmall :: Int -> Int
+myAddSmall a
+  | a <= 100  = 0
+  | otherwise = a
+
+myAddSmall' :: Int -> Int -> Int
+myAddSmall' a b
+  | a <= 100  = 0
+  | otherwise = x + y
+  where x = a * b
+        y = div a b
