707f1637c902f6affc4f22f22f934280a5bcb323
Eric Tobin <eatobin@gmail.com>
2015-12-28 16:25:14 -0600

    start of ch 5

--- src/learn-you/ch05/my05.hs
+++ src/learn-you/ch05/my05.hs
@@ -1,1 +1,16 @@
 module My05 where
+
+multiThree :: Int -> Int -> Int -> Int
+multiThree x y z = x * y * z
+
+compareWithHundred :: Int -> Ordering
+compareWithHundred x = compare 100 x
+
+compareWithHundred' :: Int -> Ordering
+compareWithHundred' = compare 100
+
+divideByTen :: (Floating a) => a -> a
+divideByTen = (/ 10)
+
+isUpperAlphanum :: Char -> Bool
+isUpperAlphanum a = elem a ['A'..'Z']
