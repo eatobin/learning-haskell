79c40e21ff07b36c02c5868307400c75c4560074
Eric Tobin <eatobin@gmail.com>
2016-02-13 15:00:12 -0600

    beginning

---
+++ src/beginning-haskell/Execise2.hs
@@ -0,0 +1,19 @@
+module Exercise2 where
+
+ex0 = True : True : []
+ex1 = if not (null ["hello","hola"]) then (head ["hello","hola"]) else "empty"
+
+coll  = []
+coll1 = [[], ['v','j']]
+coll2 = [['h'], ['v','j']]
+coll3 = ["", "Eric"]
+
+whatIsEmpty :: [[t]] -> [Char]
+whatIsEmpty []     = "Whole list is empty."
+whatIsEmpty ([]:_) = "Head is empty."
+whatIsEmpty (_:_)  = "Neither list nor head is empty."
+
+ex2 = whatIsEmpty coll
+ex3 = whatIsEmpty coll1
+ex4 = whatIsEmpty coll2
+ex5 = whatIsEmpty coll3
