502b1849c5cab13667bed1b2f3cb9ae467033046
Eric Tobin <eatobin@gmail.com>
2016-08-26 12:45:10 -0500

    monad knowledge

--- src/Monads.hs
+++ src/Monads.hs
@@ -1,7 +1,8 @@
 module Monads where
 
-import Control.Applicative
-import Control.Monad
+import           Control.Applicative
+import           Control.Monad
+import           System.Random
 
 x0 = fmap (*2) (Just 6)
 
@@ -14,3 +15,13 @@ half x = if even x
 
 x3 = liftM half (Just 6)
 x4 = liftM half (Just 5)
+
+getNth :: [a] -> Int -> Maybe a
+getNth [] i = Nothing
+getNth [x] i = Just x
+getNth xs i  = Just (xs !! i)
+
+randomNth :: [a] -> IO (Maybe a)
+randomNth []  = return Nothing
+randomNth [x] = return (Just x)
+randomNth xs  = Just . (xs !!) <$> randomRIO (0,length xs - 1)
