504c7ad94c6e88e4256110c3828493270b60fb13
Eric Tobin <eatobin@gmail.com>
2016-12-12 21:17:15 -0800

    progress

--- src/MyAeson.hs
+++ src/MyAeson.hs
@@ -4,14 +4,19 @@ module MyAeson where
 
 import           Data.Aeson
 import           Data.Text
-import qualified Data.Text.Lazy.Encoding as T
-import qualified Data.Text.Lazy.IO       as T
+import qualified Data.Text.Lazy.Encoding as E
+import qualified Data.Text.Lazy.IO       as I
 import           GHC.Exts
 
+-- val :: Value
+-- val = Object $ fromList [
+--   ("numbers", Array $ fromList [Number 1, Number 2, Number 3]),
+--   ("boolean", Bool True) ]
+
 val :: Value
-val = Object $ fromList [
-  ("numbers", Array $ fromList [Number 1, Number 2, Number 3]),
-  ("boolean", Bool True) ]
+val = object [
+  "boolean" .= True,
+  "numbers" .= [1,2,3::Int] ]
 
 y0 = decode "[1,2,3]" :: Maybe [Integer]
-y1 = T.putStrLn . T.decodeUtf8 . encode $ val
+y1 = I.putStrLn . E.decodeUtf8 . encode $ val
