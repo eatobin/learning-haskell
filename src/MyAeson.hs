d52866fa19b200c5a291c6c4257ff6506d429c04
Eric Tobin <eatobin@gmail.com>
2016-12-12 21:02:18 -0800

    progress

--- src/MyAeson.hs
+++ src/MyAeson.hs
@@ -4,9 +4,14 @@ module MyAeson where
 
 import           Data.Aeson
 import           Data.Text
+import qualified Data.Text.Lazy.Encoding as T
+import qualified Data.Text.Lazy.IO       as T
 import           GHC.Exts
 
 val :: Value
 val = Object $ fromList [
   ("numbers", Array $ fromList [Number 1, Number 2, Number 3]),
   ("boolean", Bool True) ]
+
+y0 = decode "[1,2,3]" :: Maybe [Integer]
+y1 = T.putStrLn . T.decodeUtf8 . encode $ val
