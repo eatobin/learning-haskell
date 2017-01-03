ef843eefb92e331857e8ee5db1ad5bc8b46746a4
Eric Tobin <eatobin@gmail.com>
2017-01-02 22:02:06 -0600

    haskell book ch 2 exercises

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,21 @@
 module Main where
 
 main :: IO ()
-main = do
+main =
   putStrLn "hello eric"
+
+waxOn :: Int
+waxOn =
+  x * 5
+    where
+      z = 7
+      y = z + 8
+      x = y ^ 2
+
+triple :: Int -> Int
+triple x =
+  x * 3
+
+waxOff :: Int -> Int
+waxOff =
+  triple
