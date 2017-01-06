a0c93e09e1988c2bffe1eb550f9213daf7a4203a
eatobin <eatobin@gmail.com>
2017-01-06 11:56:49 -0600

    exercises

--- src/Main.hs
+++ src/Main.hs
@@ -20,5 +20,33 @@ waxOff :: Int -> Int
 waxOff =
   triple
 
-curryIs :: String
-curryIs = "Curry is awesome" ++ "!"
+myString :: String
+myString = "Curry is awesome"
+
+curryIsA :: String -> String
+curryIsA s = s ++ "!"
+
+curryIsB :: String -> String
+curryIsB s = s !! 4
+
+curryIsC :: String -> String
+curryIsC s = (drop 9 s) ++ "!"
+
+thirdLetter :: String -> Char
+thirdLetter x = x !! 3
+
+letterIndex :: Int -> Char
+letterIndex x = myString !! x
+
+rvrs :: String
+rvrs =
+  drop 9 myString ++ " " ++ take 2 $ drop 6 myString ++ " " ++ take 5 myString
+
+
+module Reverse where
+
+rvrs :: String -> String
+rvrs x = drop 9 x ++ " " ++ take 2 $ drop 6 x ++ " " ++ take 5 x
+
+main :: IO ()
+main = print $ rvrs myString
