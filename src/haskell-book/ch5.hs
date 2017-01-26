27a11285971582756bad8804b1901c1bcb31acec
Eric Tobin <eatobin@gmail.com>
2017-01-25 21:23:57 -0800

    page 145

--- src/haskell-book/ch5.hs
+++ src/haskell-book/ch5.hs
@@ -39,3 +39,21 @@ ex2b _ y = y
 
 ex3 :: a -> b -> b
 ex3 _ y = y
+
+f :: Num a => a -> a -> a
+f x y = x + y + 3
+
+myConcat :: String -> String
+myConcat x = x ++ " yo"
+
+myMult :: Fractional a => a -> a
+myMult x = (x / 3) * 5
+
+myTake :: Int -> String
+myTake x = take x "hey you"
+
+myCom :: Int -> Bool
+myCom x = x > (length [1..10])
+
+myAlph :: Char -> Bool
+myAlph x = x < 'z'
