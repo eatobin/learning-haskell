b4fbe88e77a1192edcbf00781c3280d71190dd76
Eric Tobin <eatobin@gmail.com>
2017-02-17 16:58:43 -0800

    linted

--- src/haskell-book/ch3.hs
+++ src/haskell-book/ch3.hs
@@ -6,7 +6,7 @@ waxOn =
     where
       z = 7
       y = z + 8
-      x = y ^ 2
+      x = y ^ (2 :: Integer)
 
 triple :: Int -> Int
 triple x =
@@ -20,13 +20,13 @@ myString :: String
 myString = "Curry is awesome"
 
 curryIsA :: String -> String
-curryIsA s = s ++ "!"
+curryIsA s = s `mappend` "!"
 
 curryIsB :: String -> Char
 curryIsB s = s !! 4
 
 curryIsC :: String -> String
-curryIsC s = drop 9 s ++ "!"
+curryIsC s = drop 9 s `mappend` "!"
 
 thirdLetter :: String -> Char
 thirdLetter x = x !! 3
@@ -36,4 +36,4 @@ letterIndex x = myString !! x
 
 rvrs :: String
 rvrs =
-  drop 9 myString ++ " " ++ take 2 (drop 6 myString) ++ " " ++ take 5 myString
+  drop 9 myString `mappend` " " `mappend` take 2 (drop 6 myString) `mappend` " " `mappend` take 5 myString
