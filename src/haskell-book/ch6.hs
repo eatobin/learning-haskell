5111e6c1e8c25f8beddb6c3d940a103a694f3be3
Eric Tobin <eatobin@gmail.com>
2017-03-09 16:48:56 -0600

    progress

--- src/haskell-book/ch6.hs
+++ src/haskell-book/ch6.hs
@@ -2,9 +2,24 @@
 
 module Ch6 where
 
-data Trivial =
-  Trivial'
+data HiLo =
+  Hi | Lo
 
-instance Eq Trivial where
-  Trivial' == Trivial' = True
-  Trivial' /= Trivial' = False
+instance Eq HiLo where
+  (==) Hi Hi = True
+  (==) Lo Lo = True
+  (==) _ _   = False
+
+
+data Identity a =
+  Identity a
+
+instance Eq a => Eq (Identity a) where
+  (==) (Identity v) (Identity v') = v == v'
+
+
+data TisAnInteger =
+  TisAn Integer
+
+instance Eq TisAnInteger where
+ (==) (TisAn x) (TisAn x') = x == x'
