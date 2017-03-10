688e489b69981c0a76769baf67fa5ed8954380e0
Eric Tobin <eatobin@gmail.com>
2017-03-09 21:43:56 -0600

    page 179 done

--- src/haskell-book/ch6.hs
+++ src/haskell-book/ch6.hs
@@ -40,5 +40,38 @@ data StringOrInt =
 instance Eq StringOrInt where
   (==) (TisAnInt i) (TisAnInt i')     = i == i'
   (==) (TisAString s) (TisAString s') = s == s'
-  (==) _ _ = False
-  
+  (==) _ _                            = False
+
+
+data Pair a =
+  Pair a a
+
+instance Eq a => Eq (Pair a) where
+  (==) (Pair g _) (Pair _ h) = g == h
+
+
+data Tuple a b =
+   Tuple a b
+
+instance (Eq a, Eq b) => Eq (Tuple a b) where
+  (==) (Tuple c d) (Tuple c' d') =
+       c == c' && d == d'
+
+
+data Which a =
+    ThisOne a
+  | ThatOne a
+
+instance Eq a => Eq (Which a) where
+  (==) (ThisOne w) (ThatOne x) = w == x
+  (==) _ _                     = False
+
+
+data EitherOr a b =
+    Hello a
+  | Goodbye b
+
+instance (Eq a, Eq b) => Eq (EitherOr a b) where
+  (==) (Hello d) (Hello d')     = d == d'
+  (==) (Goodbye e) (Goodbye e') = e == e'
+  (==) _ _                      = False
