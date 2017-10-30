9d062cc0f6a5d35ec58a48c6328e57bd1cff8cfb
Eric Tobin <eatobin@gmail.com>
2017-10-30 15:11:31 -0500

    to artful dodgy

--- src/haskell-book/ch7.hs
+++ src/haskell-book/ch7.hs
@@ -184,3 +184,68 @@ nums x =
     LT -> -1
     GT -> 1
     EQ -> 0
+
+-- λ> :t flip
+-- flip :: (a -> b -> c) -> b -> a -> c
+-- λ> (-) 10 1
+-- 9
+-- λ> flip (-) 10 1
+-- -9
+
+plusOne :: Integer -> Integer
+plusOne x = x + 1
+
+returnAfterApply :: (a -> b) -> a -> c -> b
+returnAfterApply f a _ = f a
+
+-- λ> returnAfterApply plusOne 16 6
+-- 17
+
+-- pg 242
+
+-- data Employee =
+--     Coder
+--   | Manager
+--   | Veep
+--   | CEO
+--   deriving (Eq, Ord, Show)
+--
+-- reportBoss :: Employee -> Employee -> IO ()
+-- reportBoss e e' =
+--   putStrLn $ show e ++
+--     " is the boss of " ++
+--     show e'
+--
+-- employeeRank :: Employee -> Employee -> IO ()
+-- employeeRank e e' =
+--   case compare e e' of
+--     GT -> reportBoss e e'
+--     EQ -> putStrLn "Neither employee is the boss"
+--     LT -> reportBoss e' e
+
+data Employee =
+    Coder
+  | Manager
+  | Veep
+  | CEO
+  deriving (Eq, Ord, Show)
+
+reportBoss :: Employee -> Employee -> IO ()
+reportBoss e e' =
+  putStrLn $ show e ++
+    " is the boss of " ++
+    show e'
+
+codersRuleCEOsDrool :: Employee -> Employee -> Ordering
+codersRuleCEOsDrool Coder Coder = EQ
+codersRuleCEOsDrool Coder _ = GT
+codersRuleCEOsDrool _ Coder = LT
+codersRuleCEOsDrool e e' =
+  compare e e'
+
+employeeRank :: ( Employee -> Employee -> Ordering ) -> Employee -> Employee -> IO ()
+employeeRank f e e' =
+  case f e e' of
+    GT -> reportBoss e e'
+    EQ -> putStrLn "Neither employee is the boss"
+    LT -> reportBoss e' e
