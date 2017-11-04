f1855511607a7fc9815af7d125388f4600ce51da
eatobin <eatobin@gmail.com>
2017-11-03 23:07:05 -0500

    progress

--- src/haskell-book/ch7.hs
+++ src/haskell-book/ch7.hs
@@ -446,3 +446,14 @@ hunsD :: Integral a => a -> a
 hunsD x = d
   where xLast = (fst . divMod x) 100
         d = mod xLast 10
+
+foldBool1 :: a -> a -> Bool -> a
+foldBool1 x y b =
+  case b of
+    True -> x
+    False -> y
+
+foldBool2 :: a -> a -> Bool -> a
+foldBool2 x y b
+  | b = x
+  | otherwise = y
