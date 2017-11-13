28b7405f29de008efce015fd8f8a5b6f2fa58b6f
Eric Tobin <eatobin@gmail.com>
2017-11-13 10:48:25 -0600

    fizzBuzz

---
+++ src/fizzBuzz.hs
@@ -0,0 +1,14 @@
+{-# OPTIONS -Wall #-}
+
+module Fizzbuzz where
+
+fizzBuzz :: Integer -> String
+fizzBuzz x
+  | mod x 15 == 0 = "FizzBuzz"
+  | mod x 3 == 0 = "Fizz"
+  | mod x 5 == 0 = "Buzz"
+  | otherwise = show x
+
+fizzBuzzMap :: [String]
+fizzBuzzMap =
+  map fizzBuzz [7..16]
