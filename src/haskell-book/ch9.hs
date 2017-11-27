e8528423ee201fccef98b8614473ab3670b3cff0
Eric Tobin <eatobin@gmail.com>
2017-11-27 14:05:11 -0600

    progress

--- src/haskell-book/ch9.hs
+++ src/haskell-book/ch9.hs
@@ -1,3 +1,12 @@
 {-# OPTIONS -Wall #-}
 
 module Ch9 where
+
+safeHead :: [a] -> Maybe a
+safeHead [] = Nothing
+safeHead (x:_) = Just x
+
+safeTail :: [a] -> Maybe [a]
+safeTail [] = Nothing
+safeTail [_] = Nothing
+safeTail (_:xs) = Just xs
