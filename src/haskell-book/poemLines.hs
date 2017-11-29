ac243df1289a72998781eb4a7da583694f78b749
Eric Tobin <eatobin@gmail.com>
2017-11-29 14:43:09 -0600

    progress

--- src/haskell-book/poemLines.hs
+++ src/haskell-book/poemLines.hs
@@ -16,3 +16,22 @@ fourthSen = "Could frame thy fearful\ \ symmetry?"
 
 sentences :: String
 sentences = firstSen ++ secondSen ++ thirdSen ++ fourthSen
+
+-- Implement this
+myLines :: String -> [String]
+myLines = undefined
+
+-- What we want 'myLines sentences' -- to equal
+shouldEqual :: [String]
+shouldEqual = [ "Tyger Tyger, burning bright"
+              , "In the forests of the night"
+              , "What immortal hand or eye"
+              , "Could frame thy fearful symmetry?"
+              ]
+
+-- The main function here is a small test -- to ensure you've written your function -- correctly.
+main :: IO ()
+main =
+  print $
+  "Are they equal? "
+  ++ show (myLines sentences == shouldEqual)
