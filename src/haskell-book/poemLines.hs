9e9ea2d60849411e43b6d3f152ac20fbfbf06ee2
Eric Tobin <eatobin@gmail.com>
2017-11-29 14:35:48 -0600

    progress

---
+++ src/haskell-book/poemLines.hs
@@ -0,0 +1,18 @@
+{-# OPTIONS -Wall #-}
+
+module PoemLines where
+
+  firstSen :: String
+  firstSen = "Tyger Tyger, burning bright\n"
+
+  secondSen :: String
+  secondSen = "In the forests of the night\n"
+
+  thirdSen :: String
+  thirdSen = "What immortal hand or eye\n"
+
+  fourthSen :: String
+  fourthSen = "Could frame thy fearful\ \ symmetry?"
+
+  sentences :: String
+  sentences = firstSen ++ secondSen ++ thirdSen ++ fourthSen
