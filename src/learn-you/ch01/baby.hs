9af1b225a387233d93952387fc6e03eb964783e2
Eric Tobin <eatobin@gmail.com>
2019-03-21 15:34:56 -0500

    format and lint

--- src/learn-you/ch01/baby.hs
+++ src/learn-you/ch01/baby.hs
@@ -4,40 +4,33 @@ doubleMe x = x + x
 
 doubleUs x y = x * 2 + y * 2
 
-doubleSmallNumber x =
-  if x > 100
-  then x
-  else x * 2
+doubleSmallNumber x = if x > 100 then x else x * 2
 
-doubleSmallNumber' x =
-  (if x > 100
-  then x
-  else x * 2)
-  + 1
+doubleSmallNumber' x = (if x > 100 then x else x * 2) + 1
 
-inThere = elem 'e' "eric"
+inThere = 'e' `elem` "eric"
 -- True
 
-lineUp = ['a','c'..'k']
+lineUp = ['a', 'c' .. 'k']
 -- "acegik"
 
-listComp = [x * 2 | x <- [1..10]]
+listComp = [ x * 2 | x <- [1 .. 10] ]
 -- [2,4,6,8,10,12,14,16,18,20]
 
-listComp' = [x * 2 | x <- [1..10], x * 2 >= 12]
+listComp' = [ x * 2 | x <- [1 .. 10], x * 2 >= 12 ]
 -- [12,14,16,18,20]
 
-listComp'' = [x | x <- [50..100], mod x 7 == 3]
+listComp'' = [ x | x <- [50 .. 100], mod x 7 == 3 ]
 -- [52,59,66,73,80,87,94]
 
-boomBang xs = [if x < 10 then "Boom!" else "Bang!" | x <- xs, odd x]
-boomBangs = boomBang [7..13]
+boomBang xs = [ if x < 10 then "Boom!" else "Bang!" | x <- xs, odd x ]
+boomBangs = boomBang [7 .. 13]
 -- ["Boom!","Boom!","Bang!","Bang!"]
 
-notIt = [x | x <- [10..20], x /= 13, x /= 15, x /= 19]
+notIt = [ x | x <- [10 .. 20], x /= 13, x /= 15, x /= 19 ]
 -- [10,11,12,14,16,17,18,20]
 
-length' xs = sum [1 | _ <- xs]
+length' xs = sum [ 1 | _ <- xs ]
 
-zipFruit = zip [1..] ["apple", "orange", "cherry", "mango"]
+zipFruit = zip [1 ..] ["apple", "orange", "cherry", "mango"]
 -- [(1,"apple"),(2,"orange"),(3,"cherry"),(4,"mango")]
--- src/learn-you/ch02/myTypes.hs
+++ src/learn-you/ch02/myTypes.hs
@@ -1,33 +1,33 @@
 module MyTypes where
 
-myAbs ::  Num a => a -> a
-myAbs x = abs x
+myAbs :: Num a => a -> a
+myAbs = abs
 
 myAbs' :: Num a => (a -> a) -> a -> a
-myAbs' f x = f x
+myAbs' f = f
 -- myAbs' abs (-77) -> 77
 
 myConvert :: (a -> b) -> a -> b
-myConvert f x = f x
+myConvert f = f
 -- myConvert fromIntegral 7 :: Double -> 7.0
 
 myLength :: Foldable t => t a -> Int
-myLength xs = length xs
+myLength = length
 
 myLengthAdd :: Float
-myLengthAdd = fromIntegral (length [1,2,3,4]) + 3.2
+myLengthAdd = fromIntegral (length [1, 2, 3, 4]) + 3.2
 
 myLengthAdd' :: (Foldable t, Fractional b) => t a -> b
 myLengthAdd' xs = fromIntegral (length xs) + 3.2
 
 myHead :: [a] -> a
-myHead xs = head xs
+myHead = head
 
 myOrd :: Ord a => a -> a -> Bool
 myOrd a b = a > b
 
 myOrd' :: Ord a => a -> a -> Ordering
-myOrd' a b = compare a b
+myOrd' = compare
 
 myRead = read "5" :: Int
 
