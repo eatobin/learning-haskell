8068be9003ff73dc7897df43d46c991046aaac93
Eric Tobin <eatobin@gmail.com>
2019-03-26 14:37:29 -0500

    progress

--- src/Monads.hs
+++ src/Monads.hs
@@ -1,5 +1,18 @@
 -- http://adit.io/posts/2013-04-17-functors,_applicatives,_and_monads_in_pictures.html
 
+-- Conclusion
+-- A functor is a data type that implements the Functor typeclass.
+-- An applicative is a data type that implements the Applicative typeclass.
+-- A monad is a data type that implements the Monad typeclass.
+-- A Maybe implements all three, so it is a functor, an applicative, and a monad.
+-- What is the difference between the three?
+
+
+
+-- functors: you apply a function to a wrapped value using fmap or <$>
+-- applicatives: you apply a wrapped function to a wrapped value using <*> or liftA
+-- monads: you apply a function that returns a wrapped value, to a wrapped value using >>= or liftM
+
 module Monads where
 
 import           Control.Applicative
@@ -15,9 +28,7 @@ z5 = Just (+ 5) <*> Just 3
 
 x2 = fmap (* 5) (Just 3)
 
--- *Monads
 z = (+ 3) <$> (+ 2)
--- *Monads
 -- λ> z 6
 -- 11
 
@@ -28,6 +39,9 @@ half x = if even x then Just (x `div` 2) else Nothing
 --     Nothing >>= func = Nothing
 --     Just val >>= func  = func val
 
+noNad = half 66
+-- λ> noNad
+-- Just 33
 x3 = Just 3 >>= half
 x4 = Just 4 >>= half
 x5 = Nothing >>= half
