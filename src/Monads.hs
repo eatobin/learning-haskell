0ca4a4299f5bcd02aff03f98a419941aadc21c9e
Eric Tobin <eatobin@gmail.com>
2022-07-07 10:32:12 -0500

    add turnstile

--- src/Monads.hs
+++ src/Monads.hs
@@ -116,3 +116,39 @@ newNum = do
 -- *Monads
 -- λ> newState
 -- 2340
+
+data TurnstileState = Locked | Unlocked
+  deriving (Eq, Show)
+
+data TurnstileOutput = Thank | Open | Tut
+  deriving (Eq, Show)
+
+coin, push :: TurnstileState -> (TurnstileOutput, TurnstileState)
+
+coin _ = (Thank, Unlocked)
+
+push Locked   = (Tut , Locked)
+push Unlocked = (Open, Locked)
+
+monday :: TurnstileState -> ([TurnstileOutput], TurnstileState)
+monday s0 =
+  let (a1, s1) = coin s0
+      (a2, s2) = push s1
+      (a3, s3) = push s2
+      (a4, s4) = coin s3
+      (a5, s5) = push s4
+  in ([a1, a2, a3, a4, a5], s5)
+
+coinS, pushS :: State TurnstileState TurnstileOutput
+coinS = state coin
+pushS = state push
+
+
+mondayS :: State TurnstileState [TurnstileOutput]
+mondayS = do
+  a1 <- coinS
+  a2 <- pushS
+  a3 <- pushS
+  a4 <- coinS
+  a5 <- pushS
+  return [a1, a2, a3, a4, a5]
