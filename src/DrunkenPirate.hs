80d1237e623ad42599a3e0ee64e4e04a399a9b38
Eric Tobin <eatobin@gmail.com>
2026-07-12 15:30:23 -0700

    clean up

--- src/DrunkenPirate.hs
+++ src/DrunkenPirate.hs
@@ -32,42 +32,6 @@ runTreasureMapState = do
   msg3 <- stagger
   return [msg1, msg2, msg3]
 
--- runTreasureMapState :: State Position [String]
--- runTreasureMapState = do
---   let acc = [] :: [String]
---   currentPosition <- get
---   if currentPosition < 5
---     then do
---       msg <- crawl
---       return (msg : acc)
---     else do
---       return acc
-
--- runTreasureMapState :: State Position [String]
--- runTreasureMapState = do
---   currentPosition <- get
---   if currentPosition < 5
---     then do
---       msg <- crawl
---       return [msg]
---     else do
---       msg <- crawl
---       let newPosition = currentPosition + 1
---       put newPosition
---       runTreasureMapState
-
--- gasPlus :: Int -> Int
--- gasPlus outerModule =
---   loop outerModule 0
---   where
---     loop :: Int -> Int -> Int
---     loop innerModule acc
---       | newGas <= 0 = acc
---       | otherwise = loop newGas (acc + newGas)
---       where
---         newGas = gas innerModule
--- 5 : [1, 2, 3]
-
 main :: IO ()
 main = do
   let initialPosition = 0
